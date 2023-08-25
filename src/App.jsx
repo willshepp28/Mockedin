41cfee63a60e15e71ee1e079e287da7dfaa5e3c0
willshepp28 <willsheppard29@gmail.com>
2023-08-25 09:36:33 -0500

    created db.json to store mocked users; setup App.jsx to fetch and display mocked users

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [linkedinUsers, setLinkedinUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/linkedinUsers')
+      .then((response) => {
+        setLinkedinUsers(response.data)
+      })
+  });
+
+  console.log(linkedinUsers);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {linkedinUsers.map((user) => (
+        <div key={user.id}>{user.name} -  {user.position}</div>
+      ))}
+    </div>
   )
 }
 
